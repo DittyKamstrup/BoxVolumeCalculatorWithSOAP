09b45c19a9052ba6a000cbce022210ad2ae323ac
Ditte <Ditte@DESKTOP-QORFADN>
2017-10-29 13:20:44 +0100

    BVCConsoleApp project added to solution as Service Consumer

---
+++ BVCConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BVCConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BVCServiceReference1.Service1Client service = new BVCServiceReference1.Service1Client();
+
+            var getSideResult = service.GetSide(10, 2, 2.5);
+
+            var getVolumeResult = service.GetVolume(2, 2, 2);
+
+            Console.WriteLine("1. resultat (forventet : 2) : " + getSideResult + ", 2. resultat (forventet: 8) : " + getVolumeResult);
+        }
+    }
+}
