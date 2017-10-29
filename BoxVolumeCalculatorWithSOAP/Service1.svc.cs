3795a8d9fbc52197e1eddd8e90390c4f60ca183c
Ditte <Ditte@DESKTOP-QORFADN>
2017-10-29 16:56:23 +0100

    InsertData() & ConnectionString added to Service1

--- BoxVolumeCalculatorWithSOAP/Service1.svc.cs
+++ BoxVolumeCalculatorWithSOAP/Service1.svc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -12,6 +13,9 @@ namespace BoxVolumeCalculatorWithSOAP
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
+
+        private const string ConnectionString = "Server=tcp:dittessqlserver.database.windows.net,1433;Initial Catalog=SchoolDB;Persist Security Info=False;User ID=ditteak;Password=Ditte1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;";
+
         public double GetSide(double volume, double side1, double side2)
         {
             double length = volume / (side1 * side2);
@@ -22,6 +26,27 @@ namespace BoxVolumeCalculatorWithSOAP
         {
             double volume = length * width * height;
             return volume;
+        }
+
+        public int InsertData(string request, double volume, double length, double width, double height)
+        {
+            const string insertData = "insert into BoxVolumeCalculatorTable (time, request, volume, length, width, height) values (@time, @request, @volume, @length, @width, @height)";
+
+            using (SqlConnection databaseConnection = new SqlConnection(ConnectionString))
+            {
+
+                databaseConnection.Open();
+                using (SqlCommand insertCommand = new SqlCommand(insertData, databaseConnection))
+                {
+                    insertCommand.Parameters.AddWithValue("@request", request);
+                    insertCommand.Parameters.AddWithValue("@volume", volume);
+                    insertCommand.Parameters.AddWithValue("@length", length);
+                    insertCommand.Parameters.AddWithValue("@width", width);
+                    insertCommand.Parameters.AddWithValue("@heigth", height);
+                    int rowsAffected = insertCommand.ExecuteNonQuery();
+                    return rowsAffected;
+                }
+            }
         }
     }
 }
